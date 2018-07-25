e88fd010167c74246c05a93ef90c57e12ff20ee9
Noven Temer <noven5823@gmail.com>
2018-07-25 12:50:06 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+function findMatching(array, name){
+    return array.filter(function(item){
+    	if(item.toLowerCase() === name.toLowerCase()){
+    		return item
+    	}
+    })
+}
+function fuzzyMatch(arra, letters){
+	return arra.filter(function(item){
+		  var toStr= (item.slice(0,2)).toString()
+          if(toStr === letters){
+          	return item
+          }
+	})
+}
+function matchName (arra, name) {
+  return arra.filter(function (driver) {
+    if (driver.name.toLowerCase() === name.toLowerCase()){
+    	return driver.name;
+    }
+
+
+  });
+    }
